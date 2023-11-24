2f0035ecd857f692f418a74c411fbcb1458ecb51
CamilaFallas <camilafallasnaranjo@gmail.com>
2023-11-23 18:15:50 -0600

    feat:add margin

--- src/Tools.jsx
+++ src/Tools.jsx
@@ -17,7 +17,7 @@ const Tools = () => {
         <li><a className="link-secondary" target="_blank" rel="noreferrer" href="https://www.figma.com">Figma</a></li>
         <li><a className="link-secondary" target="_blank" rel="noreferrer" href="https://sass-lang.com/">SASS</a></li>
         <li><a className="link-secondary" target="_blank" rel="noreferrer" href="https://getbootstrap.com/">Bootstrap</a></li>
-        <li><a className="link-secondary" target="_blank" rel="noreferrer" href="https://wordpress.com/hosting/?aff=15767&cid=1654213&cmp_id=11549382845&adg_id=111353876614&kwd=wordpress&device=c&gad_source=1&gclid=CjwKCAiAxreqBhAxEiwAfGfndNPukdtVrVw2yWZIBWdI4Dy__pm-pc6ULSBO1bjWmNUtWyog0m51rRoCs_4QAvD_BwE">WORDPRESS</a></li>
+        <li><a className="link-secondary" target="_blank" rel="noreferrer" href="https://wordpress.com/hosting/?aff=15767&cid=1654213&cmp_id=11549382845&adg_id=111353876614&kwd=wordpress&device=c&gad_source=1&gclid=CjwKCAiAxreqBhAxEiwAfGfndNPukdtVrVw2yWZIBWdI4Dy__pm-pc6ULSBO1bjWmNUtWyog0m51rRoCs_4QAvD_BwE">WordPress</a></li>
       </ul>
     </section>
   );
