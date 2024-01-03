d18bfb9e564e335b0b014e9a8afcc2467466310f
CamilaFallas <camilafallasnaranjo@gmail.com>
2024-01-03 11:35:10 -0600

    add: repository link

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,5 +1,6 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
+import { IoIosArrowForward } from 'react-icons/io';
 
 const Footer = () => {
   return (
@@ -7,6 +8,9 @@ const Footer = () => {
       <p>
         This Portfolio was built with <a className="link-secondary" target="_blank" rel="noreferrer" href="https://react.dev/">React</a> - <a className="link-secondary" target="_blank" rel="noreferrer" href="https://getbootstrap.com/">Bootstrap</a> - <a className="link-secondary" target="_blank" rel="noreferrer" href="https://www.framer.com/motion/">Framer Motion</a>
       </p>
+      <a className="link-secondary" href="https://github.com/CamilaFallas/portfolio.git" target="_blank" rel="noopener noreferrer">
+        Watch Repository <IoIosArrowForward />
+      </a>
     </div>
   );
 };
