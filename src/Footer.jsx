d10a361f89258cd86ab7ffcf6206e404437490ee
CamilaFallas <camilafallasnaranjo@gmail.com>
2024-01-03 11:40:24 -0600

    style: repository link custome color

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,6 +1,6 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
-import { IoIosArrowForward } from 'react-icons/io';
+import { IoIosOpen } from 'react-icons/io';
 
 const Footer = () => {
   return (
@@ -8,8 +8,8 @@ const Footer = () => {
       <p>
         This Portfolio was built with <a className="link-secondary" target="_blank" rel="noreferrer" href="https://react.dev/">React</a> - <a className="link-secondary" target="_blank" rel="noreferrer" href="https://getbootstrap.com/">Bootstrap</a> - <a className="link-secondary" target="_blank" rel="noreferrer" href="https://www.framer.com/motion/">Framer Motion</a>
       </p>
-      <a className="link-secondary" href="https://github.com/CamilaFallas/portfolio.git" target="_blank" rel="noopener noreferrer">
-        Watch Repository <IoIosArrowForward />
+      <a className="link_custom-color" href="https://github.com/CamilaFallas/portfolio.git" target="_blank" rel="noopener noreferrer">
+        Watch Repository <IoIosOpen />
       </a>
     </div>
   );
